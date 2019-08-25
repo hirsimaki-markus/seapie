ac1a3c358f7a1192ddda2ec43b8afed832c370ff
Markus Hirsimäki <hirsimaki.markus@gmail.com>
2019-08-25 12:56:28 +0300

    v0.3 added error handling

--- seapie.py
+++ seapie.py
@@ -1,6 +1,7 @@
 import sys
 import ctypes
 import codeop
+import traceback
 
 def seapie(scope=1):
     """Scope Escaping Arbitrary Python Injection Executor"""
@@ -12,33 +13,53 @@ def seapie(scope=1):
     def single_prompt():
             accumulator = ""
             while True:
-                if not accumulator: raw_text = input(">>> ")
-                else:               raw_text = input("... ")
+                try:
+                    if not accumulator: raw_text = input(">>> ")
+                    else:               raw_text = input("... ")
+                except KeyboardInterrupt:
+                    raw_text = ""
+                    print("\nKeyboardInterrupt")
                 # next two lines catch magic values when they are entered alone
                 if accumulator == "":
                     if raw_text in ("quit", "exit", "exit()"): return "!EXIT"
                 accumulator += "\n"+raw_text
                 # add newline to as input doesnt read it. this will add extra
                 # newline at start
-                result = codeop.compile_command(accumulator)
-                if result != None: break
-            return accumulator[1:] # cut extra newline at start
+                try:
+                    result = codeop.compile_command(accumulator)
+                except SyntaxError:
+                    result = accumulator
+                if result != None: # captured one complete command
+                    try:
+                        compile(accumulator, '<stdin>', 'eval')
+                        return (accumulator[1:], 'expression')
+                    except SyntaxError:
+                        return (accumulator[1:], 'statement')
+
 
     parent_frame = sys._getframe(1)
     local_frame = sys._getframe(0)
     local_frame.f_locals.update(parent_frame.f_locals)
     ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(local_frame),ctypes.c_int(1))
+    print("SEAPIE v0.3 type !help for SEAPIE help")
     while True:
         # local variables can be assinged by modifying locals() but cannot be
         # edited nor deleted via it
 
-        codeblock = single_prompt()
+        codeblock, block_type = single_prompt()
         if codeblock == "!EXIT":
             break # this block allows reacting to magic values
-        try:
-            print(eval(codeblock)) # print all expressions
-        except SyntaxError:        # and pass statements
-            exec(codeblock)
+        if block_type == 'expression':
+            try:
+                print(eval(codeblock))
+            except Exception as error:      # so we need to catch the error
+                traceback.print_exc()       # and print traceback
+
+        if block_type == 'statement':
+            try:
+                exec(codeblock)             # that might still fail
+            except (Exception, KeyboardInterrupt) as error:      # so we need to catch the error
+                traceback.print_exc()       # and print traceback
 
         # purge updates from vairables used in this function to avoid
         # recursive namespaces and other pollution
@@ -59,3 +80,6 @@ def seapie(scope=1):
         for i in deleted_items:
             del parent_frame.f_locals[i]
             ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(parent_frame),ctypes.c_int(1))
+
+
+#     exec(compile("1;1;1","lol", "single"))
