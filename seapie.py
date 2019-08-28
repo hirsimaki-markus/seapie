dc76c8a2f515d5b486f97f55b71e5b2c82a50130
Markus Hirsimäki <hirsimaki.markus@gmail.com>
2019-08-28 15:47:02 +0300

    v0.4 added support for ; symbol
    Multiple statements and/or expressions from same line are now handled as interpreter handles them

--- seapie.py
+++ seapie.py
@@ -21,20 +21,19 @@ def seapie(scope=1):
                     print("\nKeyboardInterrupt")
                 # next two lines catch magic values when they are entered alone
                 if accumulator == "":
-                    if raw_text in ("quit", "exit", "exit()"): return "!EXIT"
+                    if raw_text == "!exit": return "!exit"
                 accumulator += "\n"+raw_text
                 # add newline to as input doesnt read it. this will add extra
                 # newline at start
+                
                 try:
                     result = codeop.compile_command(accumulator)
                 except SyntaxError:
-                    result = accumulator
+                    #result = accumulator
+                    return accumulator  # allow syntax to pass thru to pass thru
                 if result != None: # captured one complete command
-                    try:
-                        compile(accumulator, '<stdin>', 'eval')
-                        return (accumulator[1:], 'expression')
-                    except SyntaxError:
-                        return (accumulator[1:], 'statement')
+                    compiled = compile(accumulator[1:], '<stdin>', 'single') # this compilation allows for 1;1;1 to work as expected
+                    return compiled
 
 
     parent_frame = sys._getframe(1)
@@ -46,20 +45,14 @@ def seapie(scope=1):
         # local variables can be assinged by modifying locals() but cannot be
         # edited nor deleted via it
 
-        codeblock, block_type = single_prompt()
-        if codeblock == "!EXIT":
+        codeblock = single_prompt()
+        if codeblock == "!exit":
             break # this block allows reacting to magic values
-        if block_type == 'expression':
-            try:
-                print(eval(codeblock))
-            except Exception as error:      # so we need to catch the error
-                traceback.print_exc()       # and print traceback
+        try:
+            exec(codeblock)
+        except Exception as error:      # so we need to catch the error
+            traceback.print_exc()       # and print traceback
 
-        if block_type == 'statement':
-            try:
-                exec(codeblock)             # that might still fail
-            except (Exception, KeyboardInterrupt) as error:      # so we need to catch the error
-                traceback.print_exc()       # and print traceback
 
         # purge updates from vairables used in this function to avoid
         # recursive namespaces and other pollution
@@ -76,10 +69,9 @@ def seapie(scope=1):
         ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(parent_frame),ctypes.c_int(1))
         
         # find items that are locally deleted and whose deletion should be propagated to parent scope
-        deleted_items = { k : parent_frame.f_locals[k] for k in set(parent_frame.f_locals) - set(updates) }
+        deleted_items = { k : parent_frame.f_locals[k] for k in set(parent_frame.f_locals) - set(updates)}
         for i in deleted_items:
             del parent_frame.f_locals[i]
             ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(parent_frame),ctypes.c_int(1))
 
 
-#     exec(compile("1;1;1","lol", "single"))
