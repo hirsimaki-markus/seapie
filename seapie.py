14c1ee3571e6b1af7213044622c243e452481864
Markus Hirsimäki <hirsimaki.markus@gmail.com>
2019-08-30 17:20:25 +0300

    v0.5 major refactor. fixed bugs

--- seapie.py
+++ seapie.py
@@ -3,12 +3,8 @@ import ctypes
 import code
 import traceback
 
-def seapie(scope=1):
+def seapie2(scope=1):
     """Scope Escaping Arbitrary Python Injection Executor"""
-    # https://www.python.org/dev/peps/pep-0558/
-    # https://faster-cpython.readthedocs.io/mutable.html
-    # https://docs.python.org/3/library/codeop.html#codeop.compile_command
-    # https://docs.python.org/3/library/code.html#module-code
     try:
         sys.ps1
     except AttributeError:
@@ -18,73 +14,42 @@ def seapie(scope=1):
     except AttributeError:
         sys.ps2 = "... "
 
-
     def single_prompt():
-            accumulator = ""
-            while True:
-                try:
-                    if not accumulator: raw_text = input(str(sys.ps1))
-                    else:               raw_text = input(str(sys.ps2))
-                except KeyboardInterrupt:
-                    raw_text = ""
-                    print("\nKeyboardInterrupt")
-                # next two lines catch magic values when they are entered alone
-                if accumulator == "":
-                    if raw_text == "!exit": return "!exit"
-                accumulator += "\n"+raw_text
-                # add newline to as input doesnt read it. this will add extra
-                # newline at start
+        """Emulates python interactive prompt to capture one complete and valid command"""
+        accumulator = ""
+        raw_text = ""
+        while True:
+            try:
+                if not accumulator: # if on first line
+                    raw_text = input(str(sys.ps1))
+                else: # if on continuing line
+                    raw_text = input(str(sys.ps2))
+            except KeyboardInterrupt: # emulate behaviour of ctrl+c
+                print("\nKeyboardInterrupt")
+            if accumulator == "": # if reading first line
+                if raw_text == "!exit": return "!exit" # capture magic values
+            accumulator += "\n"+raw_text # input cant read newline. add it manually
 
-                try:
-                    result = code.compile_command(accumulator)
-                except SyntaxError:
-                    #traceback.print_exc()
-                    #accumulator = ""
-                    #continue
-                    return accumulator
+            try:
+                result = code.compile_command(accumulator)
+            except SyntaxError: # allow incorrect command to just pass thru
+                return accumulator
+            if result == None:
+                pass # incomplete but possibly valid command
+            else:
+                return result
 
-                if result == None:
-                    pass # incomplete but possibly valid command
-                else:
-                    return result
+    parent_frame = sys._getframe(scope)
+    parent_globals = parent_frame.f_globals
+    parent_locals = parent_frame.f_locals
 
-    
-    parent_frame = sys._getframe(1)
-    local_frame = sys._getframe(0)
-    local_frame.f_locals.update(parent_frame.f_locals)
-    ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(local_frame),ctypes.c_int(1))
     print("SEAPIE v0.3 type !help for SEAPIE help")
     while True:
-        # local variables can be assinged by modifying locals() but cannot be
-        # edited nor deleted via it
-
         codeblock = single_prompt()
         if codeblock == "!exit":
             break # this block allows reacting to magic values
         try:
-            exec(codeblock)
-        except Exception as error:      # so we need to catch the error
-            traceback.print_exc()       # and print traceback
-
-
-        # purge updates from vairables used in this function to avoid
-        # recursive namespaces and other pollution
-        updates = dict(local_frame.f_locals)
-        for i in ("injection", "frame", "__doc__", "single_prompt",
-                 "codeblock", "parent_frame", "local_frame", "updates", "scope", "i"):
-            try:
-                updates.pop(i)
-            except KeyError: # remove local variables if they exist to avoid
-                pass         # polluting namespace
-
-        # push updates to parent namespace. this cannot delete items
-        local_frame.f_locals.update(updates)
-        ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(parent_frame),ctypes.c_int(1))
-        
-        # find items that are locally deleted and whose deletion should be propagated to parent scope
-        deleted_items = { k : parent_frame.f_locals[k] for k in set(parent_frame.f_locals) - set(updates)}
-        for i in deleted_items:
-            del parent_frame.f_locals[i]
-            ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(parent_frame),ctypes.c_int(1))
-
-
+            exec(codeblock, parent_globals, parent_locals)
+            ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(parent_frame), ctypes.c_int(1))
+        except Exception as error: # catch arbitary exceptions from exec
+            traceback.print_exc()
