22e86349efdb368fa2a21efa050017f9dad61ed7
Markus Hirsimäki <hirsimaki.markus@gmail.com>
2019-09-01 13:37:48 +0300

    v0.7 added more !magic commands and better expression handling

--- seapie.py
+++ seapie.py
@@ -2,6 +2,8 @@ import sys
 import ctypes
 import code
 import traceback
+import inspect
+import codeop
 
 def seapie(scope=1):
     """Scope Escaping Arbitrary Python Injection Executor"""
@@ -26,11 +28,23 @@ def seapie(scope=1):
                     raw_text = input(str(sys.ps2))
             except KeyboardInterrupt: # emulate behaviour of ctrl+c
                 print("\nKeyboardInterrupt")
+                accumulator = ""
+                continue
             if accumulator == "": # if reading first line
                 if raw_text == "!exit": return "!exit" # capture magic values
                 if raw_text == "!scope+": return "!scope+" # capture magic values
                 if raw_text == "!scope-": return "!scope-" # capture magic values
                 if raw_text == "!help": return "!help" # capture magic values
+                if raw_text == "!tree": return "!tree" # capture magic values
+                if raw_text == "!scope": return "!scope" # capture magic values
+            else:
+                if raw_text == "": # this block should catch failing empty lines enterred after def and such
+                    try:
+                        codeop.compile_command("\n"+accumulator, "<input>", "eval")
+                    except Exception as error: # catch arbitary exceptions from exec
+                        traceback.print_exc()
+                        accumulator = ""
+                        continue
             accumulator += "\n"+raw_text # input cant read newline. add it manually
 
             try:
@@ -42,31 +56,43 @@ def seapie(scope=1):
             else:
                 return result
 
-    parent_frame = sys._getframe(scope)
-    parent_globals = parent_frame.f_globals
-    parent_locals = parent_frame.f_locals
-
     print("SEAPIE v0.5 type !help for SEAPIE help")
     while True:
+        parent_frame = sys._getframe(scope)
+        parent_globals = parent_frame.f_globals
+        parent_locals = parent_frame.f_locals
         codeblock = single_prompt()
         if codeblock == "!exit":
             break # this block allows reacting to magic values
+        if codeblock == "!scope":
+            print(sys._getframe(scope).f_code.co_name)
+            continue
         if codeblock == "!help":
-            print("SEAPIE v0.5 type !help for SEAPIE help")
-            print("commands: !help, !exit, !scope+, !scope-")
+            print("SEAPIE v0.7 type !help for SEAPIE help")
+            print("commands: !help, !exit, !scope, !scope+, !scope-, !tree")
             print("scope commands move up(-) or down(+) one call stack level")
             continue
         if codeblock == "!scope+":
-            scope += 1
-            parent_frame = sys._getframe(scope)
-            parent_globals = parent_frame.f_globals
-            parent_locals = parent_frame.f_locals
+            if scope >= 1:
+                scope -= 1
+            else:
+                print("cant go higher than current call")
             continue
         if codeblock == "!scope-":
-            scope -= 1
-            parent_frame = sys._getframe(scope)
-            parent_globals = parent_frame.f_globals
-            parent_locals = parent_frame.f_locals
+            try:
+                sys._getframe(scope+2) # skip over seapie.seapie to empty space. disallows hitting seapie. change to +1 to allow hittig seapie itself
+            except ValueError:
+                print("call stack is not deep enough")
+            else:
+                scope += 1
+            continue
+        if codeblock == "!tree":
+            for frame in reversed(inspect.stack()[1:]):
+                try:
+                    context = frame.code_context[0].strip()
+                except TypeError:
+                    context = '""'
+                print(frame.function, (10-len(str(frame.function)))*" ", "calling", context, "as ...")
             continue
         try:
             exec(codeblock, parent_globals, parent_locals)
